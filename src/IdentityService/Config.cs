f79b6849134227da249acf7b75ac2ae93d7370dd
Ulad Shuhayeu <v.shugaev03@gmail.com>
2024-10-18 12:31:36 +0200

    Configured Authentication for Postman using IdentityService

---
+++ src/IdentityService/Config.cs
@@ -0,0 +1,35 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityService;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope("jobApp", "Job app full access"),
+        };
+
+    // ONLY FOR TESTING PURPOSES USING POSTMAN
+    // This is not recommended for production
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+            {
+                ClientId = "postman",
+                ClientName = "Postman",
+                AllowedScopes = { "openid", "profile", "jobApp" },
+                RedirectUris = { "https://www.postman.com/" },
+                ClientSecrets = new[] { new Secret("NotASecret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword
+            }
+        };
+}
